fd5f2443d636e7ab298ab969e8a16e641ffd2e02
Marlon Domingos Cury <marlon.cury@edu.ufes.br>
2024-11-12 12:00:48 -0300

    Delete src directory

--- src/Lampada.java
+++
@@ -1,6 +0,0 @@
-public class Lampada implements Dispositivo {
-
-    public void ligar(){
-        System.out.println("Lampada ligada");
-    }
-}
--- src/exercicioRevisao/src/Ventilador.java
+++
@@ -1,6 +0,0 @@
-public class Ventilador implements Dispositivo {
-    @Override
-    public void ligar() {
-        System.out.println("Ventilador ligado");
-    }
-}
